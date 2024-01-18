6fa708422f4d8e1d6fe9e760aad6978b180f1098
Lunabell03 <sunnism03@gmail.com>
2024-01-18 16:33:46 +0900

    [week8] API 간단 구현까지

--- src/main/java/umc/study/apiPayload/ApiResponse.java
+++ src/main/java/umc/study/apiPayload/ApiResponse.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import umc.study.apiPayload.code.status.SuccessStatus;
 import umc.study.apiPayload.code.BaseCode;
 
 @Getter
@@ -20,20 +21,22 @@ public class ApiResponse<T> {
     private T result;
 
 
-    // 성공한 경우 응답 생성
 
-    //public static <T> ApiResponse<T> onSuccess(T result){
-    //    return new ApiResponse<>(true, SuccessStatus._OK.getCode() , SuccessStatus._OK.getMessage(), result);
-    //    }
-    //}
+
+
 
     //public static <T> ApiResponse<T> of(BaseCode code, T result){
     //       return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
    //}
 
+    // 성공한 경우 응답 생성
+    public static <T> ApiResponse<T> onSuccess(T result){
+        return new ApiResponse<>(true, SuccessStatus._OK.getCode() , SuccessStatus._OK.getMessage(), result);
+    }
+
 
     // 실패한 경우 응답 생성
     public static <T> ApiResponse<T> onFailure(String code, String message, T data){
-        return new ApiResponse<>(true, code, message, data);
+        return new ApiResponse<>(false, code, message, data);
     }
 }
